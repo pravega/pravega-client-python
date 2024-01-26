d49293e7ff5d420348a78c45d2fd5d8561b22aec
Ankur_Anand <Ankur.Anand@dell.com>
2024-01-26 14:20:29 +0530

    adding test case for delete scope
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>

--- tests/pravega_client_test.py
+++ tests/pravega_client_test.py
@@ -78,6 +78,20 @@ class PravegaTest(unittest.TestCase):
         w1.write_event("test event1")
         w1.write_event("test event2")
 
+    def test_deleteScope(self):
+        scope = ''.join(secrets.choice(string.ascii_lowercase + string.digits)
+                        for i in range(10))
+        print("Creating a Stream Manager, ensure Pravega is running")
+        stream_manager=pravega_client.StreamManager("tcp://127.0.0.1:9090", False, False)
+
+        print("Creating a scope")
+        scope_result=stream_manager.create_scope(scope)
+        self.assertEqual(True, scope_result, "Scope creation status")
+
+        print("Deleting the scope")
+        delete_scope_result=stream_manager.delete_scope(scope)
+        self.assertEqual(True, delete_scope_result, "Scope deletion status")
+
     def test_byteStream(self):
         scope = ''.join(secrets.choice(string.ascii_lowercase + string.digits)
                         for i in range(10))
