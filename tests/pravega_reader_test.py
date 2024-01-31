25974b58564125efe574eb2134588db112b90416
Ankur_Anand <Ankur.Anand@dell.com>
2024-01-31 16:36:14 +0530

    test case to read from tail
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>

--- tests/pravega_reader_test.py
+++ tests/pravega_reader_test.py
@@ -64,6 +64,47 @@ class PravegaReaderTest(aiounittest.AsyncTestCase):
         reader_group.reader_offline("reader-1")
         stream_manager.delete_reader_group("rg" + suffix, scope)
 
+    async def test_ReadfromTail(self):
+        suffix = str(random.randint(0, 100))
+        scope = "testReadFromTailScope"
+        stream = "testStream" + suffix
+        print("Creating a Stream Manager, ensure Pravega is running")
+        stream_manager = pravega_client.StreamManager("tcp://127.0.0.1:9090")
+
+        print("Creating a scope")
+        scope_result = stream_manager.create_scope(scope)
+        print(scope_result)
+        print("Creating a stream ", stream)
+        stream_result = stream_manager.create_stream(scope, stream, 1)
+        print(stream_result)
+
+        print("Creating a writer for Stream")
+        w1 = stream_manager.create_writer(scope, stream)
+
+        print("Write events")
+        w1.write_event("test event")
+        w1.write_event("test event")
+        w1.flush()
+        # Create a reader Group Configuration to read from Tail of stream.
+        rg_config = pravega_client.StreamReaderGroupConfig(True, scope, stream)
+        reader_group=stream_manager.create_reader_group_with_config("rg" + suffix, scope, rg_config)
+        r1 = reader_group.create_reader("reader-1")
+        print(repr(r1))
+        w1.write_event("test event at Tail")
+        w1.write_event("test event at Tail")
+        segment_slice = await r1.get_segment_slice_async()
+        print(segment_slice)
+        # consume the segment slice for events.
+        count=0
+        for event in segment_slice:
+            count+=1
+            print(event.data())
+            print(repr(event))
+            self.assertEqual(b'test event at Tail', event.data(), "Invalid event data")
+        self.assertEqual(count, 2, "Two events are expected")
+        reader_group.reader_offline("reader-1")
+        stream_manager.delete_reader_group("rg" + suffix, scope)
+
     async def test_asyncEventwriteAndRead(self):
         suffix = str(random.randint(0, 100))
         scope = "testAsyncEventWriteScope"
